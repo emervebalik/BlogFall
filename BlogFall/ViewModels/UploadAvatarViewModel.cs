df2fe20e882522882fecc4b6cadc8eb1f8b9cba9
emine balik <emervebalik@gmail.com>
2019-11-07 21:33:23 +0300

    Profil fotoğrafı editleme eklendi

---
+++ BlogFall/ViewModels/UploadAvatarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BlogFall.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogFall.ViewModels
+{
+    public class UploadAvatarViewModel
+    {
+        public string Photo { get; set; }
+
+
+        [Required(ErrorMessage ="Resim dosyası seçmediniz.")]
+        [ProfilePhoto(MaxFileSize = 1000000, ErrorMessage = "Resim boyutu 1 MB olmalıdır.")]
+        public HttpPostedFileBase File { get; set; }
+
+    }
+}
